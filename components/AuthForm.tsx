db21d331b0fc6bd4b73cb2426fb83df49c5b1295
Nnamdi Cyprian Ekechi <nnamdi4u09@gmail.com>
2024-06-19 16:25:55 +0100

    Implement PlaidLink and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,6 +23,7 @@ import { authFormSchema } from "@/lib/utils";
 import CustomInput from "./CustomInput";
 import { Loader2 } from "lucide-react";
 import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import PlaidLink from "./PlaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -49,20 +50,20 @@ const AuthForm = ({ type }: { type: string }) => {
       // Sign up with Appwrite & create plaid token
 
       if (type === "sign-up") {
-        // const userData = {
-        //   firstName: data.firstName!,
-        //   lastName: data.lastName!,
-        //   address1: data.address1!,
-        //   city: data.city!,
-        //   state: data.state!,
-        //   postalCode: data.postalCode!,
-        //   dateOfBirth: data.dateOfBirth!,
-        //   ssn: data.ssn!,
-        //   email: data.email,
-        //   password: data.password,
-        // };
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password,
+        };
 
-        const newUser = await signUp(data);
+        const newUser = await signUp(userData);
 
         setUser(newUser);
       }
@@ -109,8 +110,10 @@ const AuthForm = ({ type }: { type: string }) => {
         </div>
       </header>
       {user ? (
-        <div className="flex flex-col gap-4">{/* PlaidLink */}</div>
-      ) : (
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary" />
+        </div>
+      ) : ( 
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -218,7 +221,7 @@ const AuthForm = ({ type }: { type: string }) => {
             </Link>
           </footer>
         </>
-      )}
+       )}
     </section>
   );
 };
